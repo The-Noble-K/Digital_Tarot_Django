38f2cd396cb283660976fdc5ade766831c9ce7c6
The-Noble-K <jarod.sneed@gmail.com>
2019-07-14 15:00:39 -0400

    added major arcana to api

--- api/models.py
+++ api/models.py
@@ -8,5 +8,5 @@ class Card(models.Model):
     image = models.ImageField(upload_to="img/")
 
     def __str__(self):
-        return "name: %s, meaning: %s, reversal: %s, card_id: %s, image: %s" %(self.name, self.meaning, self.reversal, self.card_id, self.image)
+        return "%s, %s, %s, %s, %s" %(self.name, self.meaning, self.reversal, self.card_id, self.image)
 
